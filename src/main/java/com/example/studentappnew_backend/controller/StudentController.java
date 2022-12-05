fa60ac6a12f300c720ced87527298dcb1bda6af9
sherin200 <sherinsebastian21>
2022-12-05 14:32:54 +0530

    first

---
+++ src/main/java/com/example/studentappnew_backend/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.example.studentappnew_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class StudentController {
+    @GetMapping("/")
+    public String Homepage()
+    {
+        return "Welcome to my website";
+    }
+
+}
